67b9b381ce76826ba2c5f9a8a4cdeb2dc622e381
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-08-27 17:35:23 -0500

    ejercicio1Metodos

---
+++ src/plataforma/metodosFunciones_3/noRetorna/Gato.java
@@ -0,0 +1,19 @@
+package plataforma.metodosFunciones_3.noRetorna;
+
+/**
+ * Utilizando Replit Crea una clase con un metodo main, luego crea un metodo que no
+ * retorne valor, donde realice la impresión por la interfaz consola de un mensaje,
+ * posterior a eso llama a dicho nuevo metodo en el metodo main.
+ */
+public class Gato {
+    public static void main(String[] args) {
+        Gato miau1=new Gato();
+        miau1.ronronea();
+
+    }
+    //metodo
+    void ronronea(){
+        System.out.println("ruuuuurooo");
+
+    }
+}
