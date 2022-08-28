30459a47b88b904ec42653b09e7a034d6e88d3c3
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-08-28 18:32:55 -0500

    Assignment

---
+++ src/plataforma/javaAssignmentOperators/Assignment.java
@@ -0,0 +1,17 @@
+package plataforma.javaAssignmentOperators;
+//Los operadores de asignación se utilizan para asignar valores a las variables.
+public class Assignment {
+    public static void main(String[] args) {
+    // =
+        int x=5;
+    // +=
+       int x1=3;
+       x1+=3;
+        System.out.println(x1);
+    // -= , *= , /=,%=,&=,>>=,<<=
+
+
+    }
+
+
+}
