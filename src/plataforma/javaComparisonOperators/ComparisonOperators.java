9b89966c14723cadb3c3207e76449d3831495eb0
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-08-28 19:20:51 -0500

    Comparison Operators

---
+++ src/plataforma/javaComparisonOperators/ComparisonOperators.java
@@ -0,0 +1,19 @@
+package plataforma.javaComparisonOperators;
+
+public class ComparisonOperators {
+//Comparison operators are used to compare two values:
+    /**
+     * 1. Equal to   x==y  igual a
+     * 2. not equal  x!=y --> boolean
+     * 3. greater than x>y
+     * 4. less than  x<y
+     * 5. greater than or equal to x>=y
+     * 6. less than or equal to x<=y
+     */
+    public static void main(String[] args) {
+        int x= 5;
+        int y= 3;
+        System.out.println(x!=y);
+    }
+
+}
