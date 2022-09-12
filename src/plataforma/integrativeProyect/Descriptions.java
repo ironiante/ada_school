e7fecc30288637eea0fc2f578886d89c54eb42b5
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-09-12 07:04:16 -0500

    integrative proyect

---
+++ src/plataforma/integrativeProyect/Descriptions.java
@@ -0,0 +1,23 @@
+package plataforma.integrativeProyect;
+
+/**
+ * Bienvenid@ a la primera sesión del Proyecto Integrador, un proyecto que vas a construir de forma individual para que puedas aplicar
+ * TODOS los conocimientos que vas aprender durante esta ruta de aprendizaje para convertirte en Jr Java Backend Developer.
+ * Lee con cuidado el siguiente caso de estudio:
+ * La dueña de la tienda del barrio donde vives necesita de tu ayuda, actualmente esta llevando toda la administración de su negocio utilizando
+ * papel y lápiz, aunque a ella se le facilitan las matemáticas, le toma mucho tiempo entender el estado de su inventario y las ventas de su tienda.
+ * Para facilitar su trabajo su sobrina le ha ayudado a crear la siguiente hoja de calculo en la nube donde puedes encontrar la información de los
+ * productos del inventario:
+ * https://docs.google.com/spreadsheets/d/1YzhzQOajXheZS2Lixle1UJS_ivYfm11aY5pZX5y5zW0/edit
+ * Has sido contratado para que diseñes un programa utilizando Java, que permita a cualquier persona administrar una tienda. Para ello deberás:
+ * Crea un repositorio en Github donde almacenarás el código de tu proyecto.
+ * Crea un nuevo proyecto Java en IntelliJ Idea
+ * Crea un archivo .gitignore en la raíz de tu proyecto y agrega las siguientes extensiones para evitar subir archivos de configuración local:
+ * *.iml
+ * /.idea
+ * /out
+ * Sube los archivos de tu proyecto a el repositorio de Git crado en 1.
+ * Envía el enlace de tu repositorio en el siguiente espacio:
+ */
+public class Descriptions {
+}
